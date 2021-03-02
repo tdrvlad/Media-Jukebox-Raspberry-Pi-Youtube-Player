a8b16f6736200767b285d7bfce0b01918d7ab47c
tdrvlad <45488144+tdrvlad@users.noreply.github.com>
2021-03-02 20:55:27 +0200

    Fixed playlists

--- youtube_jukebox.py
+++ youtube_jukebox.py
@@ -1,4 +1,5 @@
 import vlc, pafy
+from pytube import Playlist
 from time import sleep
 import time
 import threading
@@ -9,9 +10,7 @@ from peripherals import LED, check_button_press
 
 rel_path = os.path.relpath(os.path.dirname(os.path.realpath(__file__)), os.getcwd()) 
 gpio_wiring_file = os.path.join(rel_path, 'gpio_wiring.yaml')
-playlists_file = os.path.join(rel_path, 'Desktop/playlists.yaml')
-
-print(playlists_file)
+themes_file = os.path.join(rel_path, 'Desktop/playlists.yaml')
 
 gpio_map = {}
 with open(gpio_wiring_file) as data_file:
@@ -21,7 +20,11 @@ GPIO.setmode(GPIO.BCM)
 
 led = LED()
 
-class Playlist:
+class Theme:
+    '''
+    Theme contains multiple Youtube Playlists URLs.
+    At startup, all the video URLs in the Theme Playlists are added into a Media Pool and then randomly selected.
+    '''
 
     def __init__(self, manager, button, name = None):
 
@@ -32,58 +35,65 @@ class Playlist:
         if pin is None:
             print('Button not defined in GPIO wiring.')
         self.button_pin = pin
-        self.urls = []
-        self.loaded_media = []
+        self.playlist_urls = []
+        self.media_pool_urls = None
         self.queue = []
 
-        self.last_media_title = None
+        '''
+        last_played assures that the random selection does not repeat itself.
+        '''
+        self.last_played = []
+        self.last_played_dim = 3
 
         if name is None:
-            self.name = 'Playlist For Button {}'.format(button)
+            self.name = 'Theme For Button {}'.format(button)
+
 
     def add_url(self, playlist_url):
-        self.urls.append(playlist_url)
+        self.playlist_urls.append(playlist_url)
+
 
     def remove_url(self, playlist_url):
-        if playlist_url in self.urls:
-            self.urls.remove(playlist_url)
+        if playlist_url in self.playlist_urls:
+           self.playlist_urls.remove(playlist_url)
     
-    def get_media(self):
-        
-        if len(self.urls) == 0:
-            print('No defined playlists.', flush = True)
 
-        check = False
-        tries = 0
-        while check == False and tries < 10:
-            tries += 1
-            playlist_url = random.choice(self.urls)
-            print('Chosen playlist URL: {}'.format(playlist_url), flush = True)
+    def load_media(self):
 
+        self.media_pool_urls = []
+
+        if len(self.playlist_urls) == 0:
+            print('No playlists in theme.', flush = True)
+
+        for playlist_url in self.playlist_urls:
             try:
-                playlist = pafy.get_playlist(playlist_url) 
-                media = random.choice(playlist['items'])
-
-                try:
-                    media_url = media['pafy'].getbestaudio().url
-                    media_title = media['pafy'].title
-
-                    print('Selected media title: {}'.format(media_title))
-
-                    if media_title != self.last_media_title:
-                        check = True
-                        self.last_media_title = media_title
-                        return media_url
-                    else:
-                        print('No same consecutive media. Rechoosing.')
-                except:
-                    media_url = None
-                    print('Error getting media from URL: {}'.format(media_url))
-                
+                playlist = Playlist(playlist_url)
+                for url in playlist.video_urls:
+                    self.media_pool_urls.append(url)
             except:
-                print('Error getting playlist from URL {}'.format(playlist_url))
+                print('Encountered error while processing playlist: {}'.format(playlist_url))
+        
+        print('Finished loading {} Playlists with a total of {} videos.'.format(len(self.playlist_urls), len(self.media_pool_urls)))
 
-        return None
+
+    def get_media(self):
+
+        if self.media_pool_urls is None:
+            self.load_media()
+        
+        if len(self.last_played) > self.last_played_dim:
+            self.last_played.pop(0)
+
+        possible_urls = [i for i in self.media_pool_urls if i not in self.last_played]
+        chosen_url = random.choice(possible_urls)
+
+        self.last_played.append(chosen_url)
+        print('Chosen media URL: {}'.format(chosen_url), flush = True)
+
+        media = pafy.new(chosen_url)
+        playable_url = media.getbest().url
+
+        return playable_url
 
 
     def play(self):
@@ -104,6 +114,7 @@ class Playlist:
                     Media.get_mrl()
                     player.set_media(Media)
                     print('Playing.', flush = True)
+                    time.sleep(1)
                     player.play()
                     check = True
                     sleep(2) 
@@ -121,42 +132,42 @@ class Playlist:
                     time.sleep(0.3)
             
 
-class PlaylistManager:
+class ThemeManager:
 
     def __init__(self):
-        self.playlists = []
+        self.themes = []
 
     def check_button_already_used(self,button):
         pin = gpio_map.get(button)
         if pin is None:
             print('Button not defined in GPIO wiring.')
-        for playlist in self.playlists:
-            if playlist.button_pin == pin:
-                return playlist
+        for playlist in self.themes:
+            if theme.button_pin == pin:
+                return theme
     
-    def add_playlist(self, button, name = None):
+    def add_theme(self, button, name = None):
 
-        playlist = Playlist(self, button, name)
-        existent_playlist = self.check_button_already_used(button)
-        if not existent_playlist is None:
-            self.playlists.remove(existent_playlist)
-        GPIO.setup(playlist.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.playlists.append(playlist)
+        theme = Theme(self, button, name)
+        existent_theme = self.check_button_already_used(button)
+        if not existent_theme is None:
+            self.themes.remove(existent_theme)
+        GPIO.setup(theme.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.themes.append(theme)
 
-        return playlist
+        return theme
     
     def deselect_all(self):
-        for playlist in self.playlists:
-            playlist.selected = False
+        for theme in self.themes:
+            theme.selected = False
 
     def check_buttons(self):
 
-        for playlist in self.playlists:
-            pressed_time = check_button_press(playlist.button_pin, led)
+        for theme in self.themes:
+            pressed_time = check_button_press(theme.button_pin, led)
             if pressed_time > 0.2:
                 self.deselect_all()
                 if pressed_time < 2:
-                    playlist.selected = True
+                    theme.selected = True
                 return True
         return False
 
@@ -164,35 +175,44 @@ class PlaylistManager:
 
         while True:
             self.check_buttons()
-            for playlist in self.playlists:
-                if playlist.selected == True:
-                    playlist.play()
+            for theme in self.themes:
+                if theme.selected == True:
+                    theme.play()
             time.sleep(0.5)
 
 
 if __name__ =='__main__':
 
-    with open(playlists_file) as data_file:
-        playlists = yaml.load(data_file, Loader=yaml.FullLoader)
+    with open(themes_file) as data_file:
+        themes = yaml.load(data_file, Loader=yaml.FullLoader)
 
-    manager = PlaylistManager()
+    manager = ThemeManager()
 
     GPIO.setup(gpio_map.get('led'), GPIO.OUT)
         
-    for playlist_name, values in playlists.items():
-        print('Adding playlist {}.'.format(playlist_name))
+    for theme_name, values in themes.items():
+        print('Adding theme {}.'.format(theme_name))
         button = values.get('button')
 
-        playlist = manager.add_playlist(
+        theme = manager.add_theme(
             button = button,
-            name = playlist_name
+            name = theme_name
         )
 
         playlists_urls = values.get('playlists_urls')
         for playlist_url in playlists_urls:
-            playlist.add_url(playlist_url)
-   
+            theme.add_url(playlist_url)
+        
+        theme.load_media()
+	
 
+    '''
+    manager.themes[0].selected = True
+    manager.themes[0].play()
+    '''
+    
     manager.run()
+
+
 
 
